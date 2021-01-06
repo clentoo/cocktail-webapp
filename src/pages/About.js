1eae27b9eaed27c375ed6157030e97dd2a3c76e9
Clinton Law <Klintonlaww@gmail.com>
2021-01-06 13:34:13 +0100

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+        ratione cumque blanditiis accusamus eius autem asperiores dolor sequi,
+        veniam quae obcaecati. Sint, culpa? Non corrupti sit doloribus expedita,
+        laborum quae magni exercitationem ab nesciunt aliquid architecto eaque
+        adipisci? Doloremque, voluptatem.
+      </p>
+    </section>
+  );
+};
+
+export default About;
